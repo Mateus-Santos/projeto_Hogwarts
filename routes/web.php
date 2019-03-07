49c1233cfc0ed5fbfa5db25cad566193c2e138b8
Leonardo Brandão <econoel@gmail.com>
2019-03-07 17:25:21 -0300

    adicionadas as telas de cadastro de sala e alocacao de sala

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,7 @@ Route::view('/tela_principal','tela_principal');
 Route::view('/registro_funcionario', 'registro_funcionario');
 
 Route::view('/login','login');
+
+Route::view('/sala', 'sala');
+
+Route::view('/alocacao-sala', 'alocacao-sala');
